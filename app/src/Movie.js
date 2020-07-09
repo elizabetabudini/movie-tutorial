9c21f3fd364f8619af383e833f9cfebbd8be240a
elizabetabudini <elizabeta.budini@studio.unibo.it>
2020-07-09 16:08:37 +0100

    Functional stateless component tutorial #14

--- app/src/Movie.js
+++ app/src/Movie.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-    static propTypes = {
-        movie: PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            
-        }),
-        desc: PropTypes.string
-    }
-    static defaultProps = {
-        desc: 'Plot not available'
-    }
-    render () {
-        return (
-            <div>
-              <h3>{this.props.movie.title}</h3>
-              <p>{this.props.desc}</p>
-            </div>
-        )
-    }
+const Movie = ({movie, desc}) => (
+    <div>
+        <h3>{movie.title}</h3>
+        <p>{desc}</p>
+    </div>
+);
+export default Movie;
+
+Movie.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        
+    }).isRequired,
+    desc: PropTypes.string
+}
+Movie.defaultProps = {
+    desc: 'Plot not available'
 }
