3f3938c2d0d18ad63389e6b2c3adabbf93090d84
elizabetabudini <elizabeta.budini@studio.unibo.it>
2020-07-09 14:19:34 +0100

    add simple movie list

--- app/src/App.js
+++ app/src/App.js
@@ -1,44 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+
+const movies = [
+  {
+    id: 1,
+    title: 'start wars'
+  },
+  {
+    id: 2,
+    title: 'spider man'
+  },
+  {
+    id: 3,
+    title: 'sherlock'
+  },
+];
 
 class App extends Component {
-
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState(
-      { input : event.target.value.trim()}
-    )
-  }
-
-  submit = () => {
-   //console.log(this.text.value)
-  }
 
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <h3>{this.state.input}</h3>
-          <input type='text' onChange={this.updateInput} value={this.state.input}></input>
-          <input type='text' ref={(input)=> this.text = input}></input>
-          <button onClick={this.submit}>show value</button>
         </header>
+        {movies.map((movie, i)=> (
+            <Movie key={i} movie={movie}/>
+          )
+        )}
       </div>
     );
   }
